c00be38585508eb9e1ae37a595fed8d35e51f046
yahya akli <akli.yahya05@gmail.com>
2024-06-15 10:43:42 +0100

    yahya

---
+++ public/build/assets/Checkbox-CIxZT8SI.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-CJE6rz52.js";function s({className:r="",...o}){return a.jsx("input",{...o,type:"checkbox",className:"rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 "+r})}export{s as C};
